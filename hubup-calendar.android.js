74512b505f72e300bbd50e05bd23d66fe81c3ac6
echap <chapreri@gmail.com>
2017-04-12 14:18:14 +0200

    removing .idea

--- hubup-calendar.android.js
+++ hubup-calendar.android.js
@@ -1,12 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var stack_layout_1 = require("ui/layouts/stack-layout");
+var content_view_1 = require("ui/content-view");
 var HubupCalendar = (function (_super) {
     __extends(HubupCalendar, _super);
     function HubupCalendar() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
+    Object.defineProperty(HubupCalendar.prototype, "android", {
+        get: function () {
+            return this._android;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(HubupCalendar.prototype, "_nativeView", {
+        get: function () {
+            return this._android;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    HubupCalendar.prototype._createUI = function () {
+        this._android = new android.widget.CalendarView(this._context);
+        if (!this._androidViewId) {
+            this._androidViewId = android.view.View.generateViewId();
+        }
+        this._android.setId(this._androidViewId);
+    };
     return HubupCalendar;
-}(stack_layout_1.StackLayout));
+}(content_view_1.ContentView));
 exports.HubupCalendar = HubupCalendar;
 //# sourceMappingURL=hubup-calendar.android.js.map
--- hubup-calendar.ios.js
+++ hubup-calendar.ios.js
@@ -1,12 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var stack_layout_1 = require("ui/layouts/stack-layout");
+var content_view_1 = require("ui/content-view");
 var HubupCalendar = (function (_super) {
     __extends(HubupCalendar, _super);
     function HubupCalendar() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
+    Object.defineProperty(HubupCalendar.prototype, "ios", {
+        get: function () {
+            return this._ios;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(HubupCalendar.prototype, "_nativeView", {
+        get: function () {
+            return this._ios;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return HubupCalendar;
-}(stack_layout_1.StackLayout));
+}(content_view_1.ContentView));
 exports.HubupCalendar = HubupCalendar;
 //# sourceMappingURL=hubup-calendar.ios.js.map
